fd4f82d5aaadda74e95dbd21d37d3cec175171f3
waheb-benzaid <waheb.benzaid@gmail.com>
2023-07-10 20:35:00 +0100

    add main folder for each modul

---
+++ Section2/datatypes.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Section 2 : Data types and control structure")
+
+}
---
+++ Section4/api.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Introduction to API development with GO")
+}
